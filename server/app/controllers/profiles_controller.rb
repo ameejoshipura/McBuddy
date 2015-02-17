2978e95dadd4d5885f6a00f7c7260c03b680c086
Ryan Ordille <ryanordille@gmail.com>
2015-02-17 13:40:27 -0500

    add show profile method

--- server/app/controllers/profiles_controller.rb
+++ server/app/controllers/profiles_controller.rb
@@ -4,11 +4,20 @@ class ProfilesController < ApplicationController
   end
 
   def create
+    @profile = Profile.new(create_profile_params)
+    @profile.save
+    redirect_to @profile
   end
 
   def edit
   end
 
   def show
+    @profile = Profile.find(params[:id])
   end
+
+  private
+    def create_profile_params
+      params.require(:profile).permit(:name, :bio)
+    end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'profile/home'
+  get 'profiles/home'
   get 'matching/home'
-  get 'profile/new'
+  get 'profiles/new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
